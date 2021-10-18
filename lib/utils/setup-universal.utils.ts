bc6795aca3035fadaa942f298950790381682b42
Boetzkes, Yannick <code@yannickboetzkes.com>
2021-10-18 21:36:50 +0200

    feat(*): add request to get method

--- lib/utils/setup-universal.utils.ts
+++ lib/utils/setup-universal.utils.ts
@@ -18,6 +18,7 @@ export function setupUniversal(
     if (cacheOptions.isEnabled) {
       const cacheKeyGenerator = cacheOptions.keyGenerator;
       cacheKey                = cacheKeyGenerator.generateCacheKey(options.req);
+      // todo add request to get method parameters
       const cacheHtml         = await ngStorageProvider.get(cacheKey);
       if (cacheHtml) {
         return callback(null, cacheHtml);
